1bb7d57d1124e8e9c680ad6db5d0e02aaf2a5125
sovnya <enp7s1@tutanota.com>
2024-08-03 17:03:03 +0200

    Add missing `strings.split` function
    This function was added in CC: Tweaked 1.112.0

--- library/cc/strings.lua
+++ library/cc/strings.lua
@@ -19,6 +19,21 @@ function strings.wrap(text, width) end
 ---@param line string The string to normalize
 ---@param width? number The width to constrain to. Defaults to the width of the terminal
 ---@return string normalizedString The string with the specified width
+---
+---[Official Documentation](https://tweaked.cc/library/cc.strings.html#v:ensure_width)
 function strings.ensure_width(line, width) end
+
+---Split a string into parts, each separated by a deliminator.
+---
+---For instance, splitting the string `"a b c"` with the deliminator `" "`, would return a table with three strings: `"a"`, `"b"`, and `"c"`.
+---By default, the deliminator is given as a [Lua pattern](https://www.lua.org/manual/5.3/manual.html#6.4.1). Passing true to the plain argument will cause the deliminator to be treated as a litteral string.
+---@param str string The string to split.
+---@param deliminator string The pattern to split this string on.
+---@param plain? boolean Treat the deliminator as a plain string, rather than a pattern.
+---@param limit? number The maximum number of elements in the returned list.
+---@return string[] strings The list of split strings.
+---
+---[Official Documentation](https://tweaked.cc/library/cc.strings.html#v:split)
+function strings.split(str, deliminator, plain, limit) end
 
 return strings
